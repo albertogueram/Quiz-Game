cf58a8a18a77f9213ac925b181640095829b857c
Alberto Guerrero <alberto11160@gmail.com>
2024-04-06 13:37:01 +0200

    Third Commit: Quiz Game - Decrypt questions using unescape method of html library + Create first version of the GUI using Tkinter

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
     question_bank.append(Question(question['question'], question['correct_answer']))
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface()
+
+#while quiz.still_has_questions():
+#    quiz.next_question()
 
 
 print(f"Your score is {quiz.score}/{len(question_bank)}")
--- ui.py
+++ ui.py
@@ -1,2 +1,30 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz Game")
+        self.window.config(width=350,height=500)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text ="Score: ", font=("Arial", 10, "bold"),
+                                 bg=THEME_COLOR, highlightthickness=0, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question = self.canvas.create_text(150, 125, text="PRUEBA",
+                                                font=("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+
+        right_image = PhotoImage(file="images/true.png")
+        self.right_btn = Button(image=right_image, bg=THEME_COLOR, highlightthickness=0)
+        self.right_btn.grid(column=1, row=2)
+
+        wrong_image = PhotoImage(file="images/false.png")
+        self.wrong_btn = Button(image=wrong_image, bg=THEME_COLOR, highlightthickness=0)
+        self.wrong_btn.grid(column=0, row=2)
+
+        self.window.mainloop()
